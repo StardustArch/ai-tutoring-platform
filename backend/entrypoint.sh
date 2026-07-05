cff9a94a42b21ad4588dec4fe3be5edd167bb453
StardustArch <paulojoaocandrinho4@gmail.com>
2026-07-05 22:59:11 +0200

    go back

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ echo "⏳ Running database migrations..."
 npx prisma migrate deploy
 
 echo "🌱 Populando banco..."
-npx ts-node prisma/seed.ts
+npx tsx prisma/seed.ts
 
 echo "🚀 Starting NestJS dev server..."
 exec npm run start:dev
